975a8f31c6761d190b1c304c295050abccc45e6a
gshah2020 <gaurang.r.shah@gmail.com>
2023-07-01 00:40:01 -0400

    next.config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const config = {
+  reactStrictMode: true,
+
+  /**
+   * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
+   * out.
+   *
+   * @see https://github.com/vercel/next.js/issues/41980
+   */
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
+  images: {
+    domains: ['cdn.jsdelivr.net', 'images.unsplash.com', 's3.amazonaws.com'],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // ensure server modules resolution does not throw on the client
+      config.resolve.fallback.fs = false;
+      config.resolve.fallback.path = false;
+    }
+    return config;
+  },
+};
+
+
+module.exports = config;
